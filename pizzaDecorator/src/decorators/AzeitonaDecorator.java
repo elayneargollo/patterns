4d4b914750b5c5462509ce6cfb2b65323b57a1c8
Elayne Natália <elayne.argollo@solutis.com.br>
2024-03-07 14:41:05 -0300

    fix patterns

---
+++ pizzaDecorator/src/decorators/AzeitonaDecorator.java
@@ -0,0 +1,16 @@
+package decorators;
+
+import interfaces.PizzaComponent;
+import interfaces.PizzaDecorator;
+
+public class AzeitonaDecorator extends PizzaDecorator {
+	
+    public AzeitonaDecorator(PizzaComponent decorated) {
+        this.decorated = decorated;
+    }
+    
+    public void preparar() {
+        decorated.preparar();
+        System.out.println("Colocando azeitona");
+    }
+}
